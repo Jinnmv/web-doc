cd1ddbb3c62a163c7089e2f979a4f7fd86d1408d
Anton Savenko <jinn.mv@gmail.com>
2014-01-12 00:30:19 +0200

    Basic FS navigation implemented

--- public/js/socketio.js
+++ public/js/socketio.js
@@ -2,6 +2,65 @@ $(document).ready(function(){
 
   var socket = io.connect();
   socket.on('connect', function(){
+    console.log("connected");
+
+    socket.on('renderNav', function(data){
+      renderNavDirs('#nav-dirs', data.dirs);
+      renderNavFiles('#nav-files', data.files, data.hideFilesExt);
+      renderBackLink('#nav-back', data.prevDir);
+    });
+
+    socket.on('renderDoc', function(data){
+      $('#doc').html(data.doc);
+      $('#doc-footer').html('<p>Last Modified: ' + data.date + '</p><hr>');
+    });
+
 
   });
+
+  $(document).on('click', '#nav-files a', function(fileLink){
+    socket.emit('readDoc', { fileName: fileLink.currentTarget.attributes['value'].value });
+  });
+
+  $(document).on('click', '#nav-dirs a', function(dirLink){
+      socket.emit('navigateToDir', { dirName: dirLink.currentTarget.attributes['value'].value });
+  });
+
+  $(document).on('click', '#nav-back a', function(backLink){
+      socket.emit('navigateBack', { dirName: decodeURIComponent(backLink.currentTarget.attributes['value'].value) });
+  });
+
+
+  function renderBackLink(container, backDirName) {
+    // clear container data
+    $(container).html('');
+
+    if (backDirName.length != 0) {
+      $(container).html('<a href="#" value="'  + encodeURIComponent(backDirName) + '">Back</a>');
+    }
+  }
+
+  function renderNavDirs(container, dirs) {
+    // clear container
+    $(container).html('');
+
+    dirs.forEach( function(item) {
+      $(container).prepend('<li><a href="#" value="' + item + '">' + item + '</a></li>');
+    });
+  }
+
+  function renderNavFiles(container, files, hideFilesExt) {
+    // clear container
+    $(container).html('');
+
+    files.forEach( function(item) {
+      //$(container).prepend('<li><a href="#" value="' + item + '">' + hideFilesExt ? trimFileExt(item) : item + '</a></li>');
+      var name = hideFilesExt ? trimFileExt(item) : item;
+      $(container).prepend('<li><a href="#" value="' + item + '">' + name + '</a></li>');
+    });
+  }
+
+  function trimFileExt(fileName) {
+    return fileName.replace(/(.*)\.[^.]+$/, '$1');
+  }
 });
--- webdoc.js
+++ webdoc.js
@@ -41,20 +41,21 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-var indexFileContent = fs.readFileSync("./doc/index.md", "utf-8");
+
+var startDir = "doc";
 
 //app.get('/', routes.index);
 app.get('/', function(req, res){
   console.log(dirContent.getDirs("doc"));
   console.log(dirContent.getFiles("doc"));
+  console.log("Reguest:", req);
   res.render('index', { title: 'Web Doc',
                        dirs: dirContent.getDirs("doc"),
                        files: dirContent.getFiles("doc"),
-                       indexMd: marked(indexFileContent),
                        mtime: fs.statSync("./doc/index.md").mtime});
 });
 
-app.get('/users', user.list);
+//app.get('/users', user.list);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
@@ -64,8 +65,81 @@ io = socketio.listen(server);
 io.set('log level', 2);
 
 io.sockets.on('connection', function (socket){
+  var prevDir = '';
+  var currentDir = '';
+  var dirs = [];
+  var files = [];
+  var depth = 0;
+  var indexFile = false;
 
-    socket.on('disconnect', function(){
+  prevDir = currentDir;
+  currentDir = startDir + '/';
+  changeDir(currentDir, prevDir);
+
+
+  /*socket.emit('renderNavDirs', { dirs: dirs } );
+  socket.emit('renderNavFiles', { files: files, hideFilesExt: true }); // TODO put hide Files Ext to config
+
+  if (indexFile) {
+    sendMdFile('index.md');
+  }*/
+
+  socket.on('readDoc', function (data){
+    // Check for file exist in current dir
+    if(files.indexOf(data.fileName) > -1){
+      sendMdFile(data.fileName);
+    }
+  });
+
+  socket.on('navigateToDir', function(data){
+    // Check for file exist in current dir
+    if(dirs.indexOf(data.dirName) > -1){
+
+      prevDir = currentDir;
+      currentDir = currentDir + data.dirName + '/';
+      changeDir(currentDir, prevDir);
+    }
+  });
+
+  socket.on('navigateBack', function(data){
+    prevDir = data.dirName.replace(/([^\/]*\/)$/,'');
+    currentDir = data.dirName;
+    changeDir(currentDir, prevDir);
+  });
+
+  socket.on('disconnect', function(){
 
   });
+
+  function changeDir(dirName, prevDir) {
+
+    dirs = dirContent.getDirs(dirName);
+    files = dirContent.getFiles(dirName);
+    indexFile = (files.indexOf("index.md") != -1); // if 'index.md' is in current dir;
+    console.log('Curren Dir:', dirName);
+    console.log('Prev Dir:', prevDir);
+    console.log('dirs', dirs);
+    console.log('files', files);
+
+
+    socket.emit('renderNav', { dirs: dirs,
+                               files: files,
+                               prevDir: prevDir,
+                               hideFilesExt: true  } ); // TODO put hide Files Ext to config
+
+    if (indexFile) {
+      sendMdFile('index.md');
+    } else {
+      socket.emit('renderDoc', { doc: ''});
+    }
+  }
+
+  function sendMdFile(fileName) {
+    socket.emit('renderDoc', { doc: renderMdFile(fileName),
+                              date: fs.statSync(currentDir + fileName).mtime });
+  }
+
+  function renderMdFile(fileName) {
+    return marked(fs.readFileSync( currentDir + fileName, 'utf-8')); // TODO move encoding to config
+  }
 });
